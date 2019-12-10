a06ee61e75f07dc74918cfdc9a08af4765079d2d
Роман <Rtsunskiy@aplana.com>
2019-12-10 22:47:02 +0300

    Добавил список друзей.

---
+++ app/src/main/java/com/example/vksbermvvm/data/modelFriends/City.java
@@ -0,0 +1,12 @@
+package com.example.vksbermvvm.data.modelFriends;
+
+import com.google.gson.annotations.SerializedName;
+
+public class City {
+
+    @SerializedName("id")
+    public Integer id;
+    @SerializedName("title")
+    public String title;
+
+}
---
+++ app/src/main/java/com/example/vksbermvvm/data/modelFriends/Friends.java
@@ -0,0 +1,10 @@
+package com.example.vksbermvvm.data.modelFriends;
+
+import com.google.gson.annotations.SerializedName;
+
+public class Friends {
+
+    @SerializedName("response")
+    public ResponseFriends response;
+
+}
